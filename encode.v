879d604cc9cb2daf02c91f89473b228b15f690cd
Ramprakash <49452779+CodePurble@users.noreply.github.com>
2021-09-14 13:27:06 +0530

    wip(encode): pass only P part of G to module
    Instead of passing the whole G matrix, since it is known that the first k columns of G are part of I_k, only passing the remaining columns (ie. P) is sufficient.

--- encode.v
+++ encode.v
@@ -7,25 +7,28 @@ parameter N = 6;
 parameter K = 3;
 
 input [K-1:0] info_bits;
-input [(K-1)*(N-1):0] generator;
+input [(K-1)*(N-K-1):0] generator_p;
 output [N-1:0] codeword;
 
-reg internal_gen [0:K-1][0:N-1];
+reg internal_gen_p [0:K-1][0:N-K-1];
 
 initial
 begin
-    integer i, j;
-    for(i = 0; i < K; i += 1) begin
-        for(j = 0; j < N; j += 1) begin
-            internal_gen[i][j] = generator[i*N + j];
+    integer i, j, k;
+    for(i = 0; i < K; i+=1) begin
+        for(j = 0; j < N-K; j+=1) begin
+            internal_gen_p[i][j] = generator[i*(N-K) + j];
         end
     end
-    for(i = 0; i < K; i += 1) begin
-        for(j = 0; j < N; j += 1) begin
-            $display("gen[%1d][%1d] = %x", i, j, internal_gen[i][j]);
+    for(i = 0; i < N-K; i+=1) begin
+        assign codeword[i+K] = 0;
+        for(j = 0; j < K; j+=1) begin
+            assign codeword[i] = codeword[i] ^ (info_bits[j] & internal_gen_p[i][j];
         end
     end
 end
+
+
 endmodule
 
 module tb;
