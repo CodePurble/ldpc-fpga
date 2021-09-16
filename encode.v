d4c65d383687c1c61d0ab7b8a42e883e97ee77ee
Ramprakash C <49452779+CodePurble@users.noreply.github.com>
2021-09-16 10:49:59 +0530

    implement encoder logic, need to test now

--- encode.v
+++ encode.v
@@ -1,43 +1,50 @@
 module encode(
     info_bits,
     generator_p,
-    codeword
+    codeword,
+    clk,
+    i_en
 );
 parameter N = 6;
 parameter K = 3;
 
+input clk, i_en;
 input [K-1:0] info_bits;
 input [(K-1)*(N-K-1):0] generator_p;
-output [N-1:0] codeword;
+output reg [N-1:0] codeword;
 
 reg internal_gen_p [0:K-1][0:N-K-1];
 reg [N-K-1:0] check_bits;
 
-initial
+always @(posedge clk)
 begin
-    integer i, j, k;
-    for(i = 0; i < K; i+=1)
+    if(i_en)
     begin
-        for(j = 0; j < N-K; j+=1)
+        integer i, j, k;
+        for(i = 0; i < K; i+=1)
         begin
-            internal_gen_p[i][j] = generator_p[i*(N-K) + j];
+            for(j = 0; j < N-K; j+=1)
+            begin
+                internal_gen_p[i][j] <= generator_p[i*(N-K) + j];
+            end
         end
-    end
-    for(i = 0; i < N-K; i+=1) begin
-        check_bits[i] = 0;
-        for(j = 0; j < K; j+=1) begin
-            check_bits[i] ^= info_bits[j] & internal_gen_p[j][i];
+        for(i = 0; i < N-K; i+=1) begin
+            check_bits[i] = 0;
+            for(j = 0; j < K; j+=1) begin
+                check_bits[i] <= check_bits[i] ^ (info_bits[j] & internal_gen_p[j][i]);
+            end
         end
+        codeword <= {info_bits, check_bits};
     end
+    else codeword <= codeword;
 end
-
-assign codeword = {info_bits, check_bits};
 endmodule
 
 module tb;
 parameter N = 6;
 parameter K = 3;
 
+reg clk, i_en;
 reg [K-1:0] info_bits;
 reg [(K-1)*(N-K-1):0] generator_p;
 wire [N-1:0] codeword;
@@ -52,15 +59,24 @@ begin
     end
 end
 
-encode #(.N(N), .K(K)) uut(.info_bits(info_bits), .generator_p(generator_p), .codeword(codeword));
+encode #(.N(N), .K(K)) uut(
+    .info_bits(info_bits),
+    .generator_p(generator_p),
+    .codeword(codeword),
+    .clk(clk),
+    .i_en(i_en)
+);
+
+initial clk = 1'b1;
+always #10 clk = ~clk;
 
 initial
 begin
     $dumpfile("encode_dump.vcd");
     $dumpvars(0, tb);
+    i_en = 1;
     info_bits = 3'b010;
-    #10;
-    $finish;
+    #30 $finish;
 end
 
 initial
