f11750ca2859a1564899a0527668d5adc1c533c9
Ramprakash Chandrashekar <49452779+CodePurble@users.noreply.github.com>
2021-09-16 09:57:14 +0530

    wip(encode): fix port errors, progress on encoding

--- encode.v
+++ encode.v
@@ -1,6 +1,6 @@
 module encode(
     info_bits,
-    generator,
+    generator_p,
     codeword
 );
 parameter N = 6;
@@ -11,24 +11,27 @@ input [(K-1)*(N-K-1):0] generator_p;
 output [N-1:0] codeword;
 
 reg internal_gen_p [0:K-1][0:N-K-1];
+reg [N-K-1:0] check_bits;
 
 initial
 begin
     integer i, j, k;
-    for(i = 0; i < K; i+=1) begin
-        for(j = 0; j < N-K; j+=1) begin
-            internal_gen_p[i][j] = generator[i*(N-K) + j];
+    for(i = 0; i < K; i+=1)
+    begin
+        for(j = 0; j < N-K; j+=1)
+        begin
+            internal_gen_p[i][j] = generator_p[i*(N-K) + j];
         end
     end
     for(i = 0; i < N-K; i+=1) begin
-        assign codeword[i+K] = 0;
+        check_bits[i] = 0;
         for(j = 0; j < K; j+=1) begin
-            assign codeword[i] = codeword[i] ^ (info_bits[j] & internal_gen_p[i][j];
+            check_bits[i] ^= info_bits[j] & internal_gen_p[j][i];
         end
     end
 end
 
-
+assign codeword = {info_bits, check_bits};
 endmodule
 
 module tb;
@@ -36,7 +39,7 @@ parameter N = 6;
 parameter K = 3;
 
 reg [K-1:0] info_bits;
-reg [(K-1)*(N-1):0] generator;
+reg [(K-1)*(N-K-1):0] generator_p;
 wire [N-1:0] codeword;
 
 initial
@@ -44,12 +47,12 @@ begin
     integer i, j;
     for(i = 0; i < K; i += 1) begin
         for(j = 0; j < N; j += 1) begin
-            generator[i*N + j] = 1'b0;
+            generator_p[i*N + j] = 1'b0;
         end
     end
 end
 
-encode #(.N(N), .K(K)) uut(.info_bits(info_bits), .generator(generator), .codeword(codeword));
+encode #(.N(N), .K(K)) uut(.info_bits(info_bits), .generator_p(generator_p), .codeword(codeword));
 
 initial
 begin
